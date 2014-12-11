ce68cde34a2dd8ac99f0325fe191cc8b845b39cf
answer123 <wcc714146815@163.com>
2014-12-12 00:51:24 +0800

    更新杨柳、闫琰部分后台代码

--- AnswerMe/Home/Controller/HistoryPageController.class.php
+++ AnswerMe/Home/Controller/HistoryPageController.class.php
@@ -8,38 +8,80 @@ namespace Home\Controller;
 use Think\Controller;
 class HistoryPageController extends Controller {
     public function index(){
-        //从session中取得uid
-        $uid = session('uid');
-
-        $course = M('course');
-        $homework = M('homework');
-        $homeworkindex = M('homeworkindex');
-        $edithw = M('edithw');
-        $contain = M('contain');
-    	$edithwfind = $edithw->where('uid='.$uid)
-    		->order('edit_time desc')
-    		->field('hid, version')
-    		->select();
-
-    	$hwfind = array();
-        for ($i=0; $i < sizeof($edithwfind); $i++) {
-            $hwfind[$i] = $homework->where($edithwfind[$i])
-            	->field('hid, version, title, update_time')
-            	->find();
-            $homeworkindexfind = $homeworkindex->where('hid='.$edithwfind[$i]['hid'])
-            	->field('like_number')
-            	->find();
-            $containfind = $contain->where('hid='.$edithwfind[$i]['hid'])
-            	->field('cid')
-            	->find();
-            $coursefind = $course->where('cid='.$containfind['cid'])
-            	->field('course_name')
-            	->find();
-            $hwfind[$i]['like_number'] = $homeworkindexfind['like_number'];
-            $hwfind[$i]['course_name'] = $coursefind['course_name'];
-    	}
-
-    	$this->assign('homeworks', $hwfind);
         $this->display();
+    }
+
+    public function findhistory() {
+    	$flag = I('post.flag');
+    	//从session中取得uid
+	    $uid = session('uid');
+
+	    $course = M('course');
+
+    	if ($flag == 1) {
+    		//课程历史
+	        $homework = M('homework');
+	        $homeworkindex = M('homeworkindex');
+	        $edithw = M('edithw');
+	        $contain = M('contain');
+	    	$edithwfind = $edithw->where('uid='.$uid)
+	    		->order('edit_time desc')
+	    		->field('hid, version')
+	    		->select();
+
+	    	$hwfind = array();
+	        for ($i=0; $i < sizeof($edithwfind); $i++) {
+	            $hwfind[$i] = $homework->where($edithwfind[$i])
+	            	->field('hid, version, title, update_time')
+	            	->find();
+	            $homeworkindexfind = $homeworkindex->where('hid='.$edithwfind[$i]['hid'])
+	            	->field('like_number')
+	            	->find();
+	            $containfind = $contain->where('hid='.$edithwfind[$i]['hid'])
+	            	->field('cid')
+	            	->find();
+	            $coursefind = $course->where('cid='.$containfind['cid'])
+	            	->field('course_name')
+	            	->find();
+	            $hwfind[$i]['like_number'] = $homeworkindexfind['like_number'];
+	            $hwfind[$i]['course_name'] = $coursefind['course_name'];
+	    	}
+
+	    	if (sizeof($hwfind) == 0) {
+	    		$dataReturn['status'] = 0;
+	    		$dataReturn['msg'] = "没有查询到数据";
+	    	} else {
+	    		$dataReturn['status'] = 1;
+	    		$dataReturn['homeworks'] = $hwfind;
+	    	}
+    	} else if ($flag == 2) {
+    		$editcourse = M('editcourse');
+    		$courseindex = M('courseindex');
+    		$editcoursefind = $editcourse->where()
+    			->order('edit_time desc')
+    			->field('cid, version')
+    			->select();
+
+    		$coursefind = array();
+    		for ($i=0; $i < sizeof($editcoursefind); $i++) { 
+    			$coursefind[$i] = $course->where($editcoursefind[$i])
+    				->field('cid, version, course_name, update_time')
+    				->find();
+    			$courseindexfind = $courseindex->where('cid='.$editcoursefind[$i]['cid'])
+    				->field('like_number')
+    				->find();
+    			$coursefind[$i]['like_number'] = $courseindexfind['like_number'];
+    		}
+
+    		if (sizeof($coursefind) == 0) {
+	    		$dataReturn['status'] = 0;
+	    		$dataReturn['msg'] = "没有查询到数据";
+	    	} else {
+	    		$dataReturn['status'] = 1;
+	    		$dataReturn['courses'] = $coursefind;
+	    	}
+    	}
+    	
+    	$this->ajaxReturn($dataReturn, 'json');
     }
 }
--- AnswerMe/Home/Controller/HomePageController.class.php
+++ AnswerMe/Home/Controller/HomePageController.class.php
@@ -3,6 +3,24 @@ namespace Home\Controller;
 use Think\Controller;
 class HomePageController extends Controller {
     public function index(){
+        $user = M('user');
+        $course = M('course');
+        $courseindex = M('courseindex');
+
+        // 贡献排行榜
+        $userfind = $user->order('experience desc')->limit(5)
+                            ->field('username')->select();
+
+        // 最热课程
+        $courseindexfind = $courseindex->order('like_number desc')->limit(3)
+                            ->select();
+        $coursefind = $course
+            ->where('cid='.$courseindexfind['cid'].' AND version='.$courseindexfind['version'])
+            ->field('course_name, teacher, brief, picture')
+            ->select();
+
+        $this->assign('userlist', $userfind);
+        $this->assign('courselist', $coursefind);
         $this->display();
     }
 }
